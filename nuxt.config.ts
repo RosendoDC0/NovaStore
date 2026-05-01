afbc0a941b16c26d87430379a5ebd0a2cade9c38
RosendoDC <rosendodc02@gmail.com>
2026-05-01 18:35:49 -0500

    fix baseURL for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/NOMBRE-DEL-REPO/'
+    baseURL: '/NovaStore/'
   },
   nitro: {
     preset: 'github_pages'
