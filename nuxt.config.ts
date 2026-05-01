a1e59984d4b0dbf2e7e6651aab2160ae5f592737
RosendoDC <rosendodc02@gmail.com>
2026-05-01 18:20:29 -0500

    setup github acctions deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/NOMBRE-DEL-REPO/'
+  },
+  nitro: {
+    preset: 'github_pages'
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
